43bb7ccf4c8e37b738b4b0a96115f93e23be180a
Jaehee Lee <jhlee838@gmail.com>
2022-08-12 23:21:16 +0900

    add description

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,13 @@ from io import BytesIO
 import streamlit as st
 import main
 
-f = st.file_uploader('Source', ['png', 'jpg', 'jpeg'], False, help=".png, .jpeg, .jpg 파일만 지원됩니다.")
+st.markdown("""
+# 굳건이 생성기
+캐릭터의 얼굴을 인식하여 굳건이 얼굴로 변환합니다.
+2D 캐릭터 얼굴을 인풋으로 넣었을 때 가장 잘 작동합니다.
+""")
+
+f = st.file_uploader('Input Image', ['png', 'jpg', 'jpeg'], False, help=".png, .jpeg, .jpg 파일만 지원됩니다.")
 
 if f is not None:
     img = BytesIO(f.read())
