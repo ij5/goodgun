e2446a1c55398b634ee17ff7c65bd7c8103e0dca
Jaehee Lee <jhlee838@gmail.com>
2022-08-12 18:52:40 +0900

    rotate to perspective transform

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from io import BytesIO, StringIO
 import cv2
 from anime_face_detector import create_detector
-from PIL import Image, ImageDraw, ImageFont
 from werkzeug.wsgi import FileWrapper
-from flask import Flask, request, Response
+from flask import Flask, request, Response, send_file
 import math
 import numpy as np
 
@@ -15,15 +14,14 @@ def get_deg(arr):
 
 detector = create_detector('yolov3', device='cpu')
 
-gg = Image.open('gg.png')
+gg = cv2.imread('gg.png', cv2.IMREAD_UNCHANGED)
+gg = cv2.cvtColor(gg, cv2.COLOR_BGRA2RGBA)
 
-def generate(image_file: BytesIO) -> BytesIO:
+def generate(image_file: BytesIO) -> bytes:
     encoded = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(encoded, cv2.IMREAD_COLOR)
+    img = cv2.imdecode(encoded, cv2.IMREAD_UNCHANGED)
     preds = detector(img)
-    
-    image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    draw = ImageDraw.Draw(image)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
 
     # for face in preds:
     #     draw.rectangle((face['bbox'][0], face['bbox'][1], face['bbox'][2], face['bbox'][3]), outline=(255, 0, 0), width=5)
@@ -35,31 +33,41 @@ def generate(image_file: BytesIO) -> BytesIO:
 
     for face in preds:
         points = face['keypoints']
-        color = image.getpixel((points[27][0], points[27][1]+20))
-        draw.polygon((
-            (points[0][0], points[0][1]),
-            (points[1][0], points[1][1]),
-            (points[2][0], points[2][1]),
-            (points[3][0], points[4][1]),
-            (points[4][0], points[4][1]),
-            (points[10][0], points[10][1]),
-            (points[9][0], points[9][1]),
-            (points[8][0], points[8][1]),
-            (points[7][0], points[7][1]),
-            (points[6][0], points[6][1]),
-            (points[5][0], points[5][1])
-        ), fill=color)
-
+        color = img[int(points[27][1]), int(points[27][0])+10]
+        polygon = np.array([
+            [points[0][0], points[0][1]],
+            [points[1][0], points[1][1]],
+            [points[2][0], points[2][1]],
+            [points[3][0], points[4][1]],
+            [points[4][0], points[4][1]],
+            [points[10][0], points[10][1]],
+            [points[9][0], points[9][1]],
+            [points[8][0], points[8][1]],
+            [points[7][0], points[7][1]],
+            [points[6][0], points[6][1]],
+            [points[5][0], points[5][1]]
+        ], np.int32)
+        cv2.fillConvexPoly(img, polygon, color=(int(color[0]), int(color[1]), int(color[2]), 255))
         deg = get_deg([points[0][0], points[0][1], points[4][0], points[4][1]])
-        rotated = gg.rotate(-deg, expand=True, center=(0,0))
-        
+        rotated = gg.copy()
         resize = math.sqrt((points[10][0] - points[5][0])**2 + (points[10][1] - points[5][1])**2)
-        rotated = rotated.resize((int(resize), int(resize*1.12)))
-        image.paste(rotated, (int(points[5][0]), int(points[5][1])), rotated)
+        rotated = cv2.resize(rotated, (int(resize), int(resize*1.12)))
+        matrix = cv2.getPerspectiveTransform(
+            np.float32([[0, 0], [rotated.shape[0],0], [0, rotated.shape[1]], [rotated.shape[0],rotated.shape[1]]]),
+            np.float32([[points[5][0], points[5][1]], [points[10][0], points[10][1]], [points[1][0], points[1][1]], [points[3][0], points[3][1]]]))
+        rotated = cv2.warpPerspective(rotated, matrix, (img.shape[1], img.shape[0]))
+
+        alpha = rotated[:, :, 3] / 255.
+        for i in range(3):
+            pointx, pointy = points[5][:2]
+            pointx, pointy = int(pointx), int(pointy)
+            img[:, :, i] = (1. - alpha) * img[0:, 0:, i] + alpha * rotated[:, :, i]
     
-    result = BytesIO()
-    image.save(result, 'PNG')
-    return result
+    
+    
+    buffer = cv2.imencode('.png', cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA))[1]
+    
+    return buffer.tobytes()
     
 
 app = Flask(__name__)
@@ -73,8 +81,6 @@ def index():
     file.save(dst)
     dst.seek(0)
     result = generate(dst)
-    result.seek(0)
-    result = FileWrapper(result)
     return Response(result, mimetype='image/png', direct_passthrough=True)
     
 
